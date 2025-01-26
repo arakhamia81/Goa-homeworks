94f48c6eda551e6b742221d6bec9c9fae5d7fa4f
arakhamia81 <nikoloziarakhamia@gmail.com>
2025-01-26 23:14:29 +0400

    second commit

---
+++ level 13/homework/homework.py
@@ -0,0 +1,16 @@
+
+number = int(input("Enter your number"))
+
+while number > 7 :
+    print(input = "chose 1 to 10")
+number = int(input("enter your number"))
+while number < 4 :
+    print("good you gusse my number")
+
+number = 4
+
+group = int(input("enter your group name"))
+print(input = "enter your groupp name")
+print(input = "yes you are right this group name is group 55")
+group = "group 52"
+
