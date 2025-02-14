1f5cb18bebb2f62922de9ca6dbc6a346a8abbc01
arakhamia81 <nikoloziarakhamia@gmail.com>
2025-02-14 14:13:17 +0400

    second commit

---
+++ level 33/classwork/classwork.py
@@ -0,0 +1,3 @@
+name = input("enter your name")
+Age = input("enter your Age")
+"Hello, + name + ! you are + age + years old"
---
+++ level 35/classwork/classwork.py
@@ -0,0 +1,7 @@
+def main():
+    simple = simple_to_float (input(simple 50))
+    sample = sample_to_float (output(simple 100))
+
+import webbrowser
+search = input("search")
+webbrowser.open("phtyon.academy" + search)
